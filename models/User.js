f499d99081c1e0b732f9ab6f56778340b13ed9d9
janudivecha <88528067+janudivecha@users.noreply.github.com>
2025-05-28 13:39:13 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: String,
-  uniqueId: { type: String, unique: true },
+  qrCode: String,
+  present: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model('User', userSchema);
